d716ae7bdb976a3748f4631326ceb162ede410bb
susiealva <susana.alvarez99@gmail.com>
2021-03-08 15:26:58 -0800

    inicializando servidor

---
+++ src/routes/rutas.js
@@ -0,0 +1,17 @@
+
+
+//framewrok de express
+const{Router} = require('express');
+//inicializar
+const router = Router();
+
+//tambien puede ser asi:
+//const express = require(express);
+//const router = express.Router();
+
+//rutas
+router.get('/', (req, res) =>{
+    res.json({"name":"Susie"});
+});
+
+module.exports = router;
